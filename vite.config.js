e13776881b8684f79a8e3db6b2d002e34c49e8a8
Yu4xr <3228906019@qq.com>
2024-12-08 09:57:55 +0800

    重新配置和构建项目

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  // 添加 base 配置，对应你的仓库名
   base: '/vuexiaoyuan/',
-  
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     }
   },
-  // 添加静态资源处理
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg']
+  build: {
+    outDir: 'dist'
+  }
 })
